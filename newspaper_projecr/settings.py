d5c10cc59c6a6b9d5ba3658873ee429c22c67152
Dima <anonim-root@mail.ru>
2020-08-19 20:15:42 +0300

    Third

--- newspaper_projecr/settings.py
+++ newspaper_projecr/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'lsgl%dktp$#@18!#g)ac2t9c$#%xfx3e0*1ur551gsl_cv2@lf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*g']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
